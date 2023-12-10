9ff877b4ca27d735a677f6e76a47ddf0474201e8
Bala Chandra Akula <115984987+BalaChandraAkula@users.noreply.github.com>
2023-12-10 12:20:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 //your code here
 
 function myFunc() {
-	let insertedValue = document.getElementById("newTodoInput").value;
-	if(insertedValue != "") {
-		let orderedList = document.getElementsByTagName("ol")[0];
-		let createList = document.createElement("li");
-		createList.textContent = insertedValue;
-		orderedList.appendChild(createList);
-		document.getElementById("newTodoInput").value = "";
-	}
+	
 }
